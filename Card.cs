7769e8e58267c895c21a4f14a559086617ca803d
RobertMcCall <robertpmccall@yahoo.com>
2016-11-30 14:04:35 -0800

    updated Peace added Cards.cs

---
+++ Card.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace Peace
+{
+    public class Card
+    {
+        public int val { get; set; }
+
+        public string suit { get; set; }
+
+        public string StringValue {
+            get{
+                switch (val)
+            }
+                    case 1:
+                        return "Ace";
+                    case 11:
+                        return "Jack";
+                    case 12:
+                        return "Queen";
+                    case 13:
+                        return "King";
+                    default:
+                        return val.ToString();
+        }
+    }
+}
+
+        public Card(int _val, string _suit)
+        {
+            val = _val;
+            suite = _suit;
+        }
+
+}
